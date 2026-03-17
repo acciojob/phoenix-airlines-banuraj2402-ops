5bb12ec939551143233305f77173276d8daec4a8
Banu <banuraj2402@gmail.com>
2026-03-17 15:02:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FlightSearch from "./components/FlightSearch";
-import FlightBooking from "./components/FlightBooking";
-import Confirmation from "./components/Confirmation";
 
-function App() {
+import FlightSearch from "./FlightSearch";
+import FlightBooking from "./FlightBooking";
+import Confirmation from "./Confirmation";
+
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<FlightSearch />} />
-        <Route path="/flight-search" element={<FlightSearch />} />
-        <Route path="/flight-booking" element={<FlightBooking />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      {/* Do not remove this div */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FlightSearch />} />
+          <Route path="/flight-search" element={<FlightSearch />} />
+          <Route path="/flight-booking" element={<FlightBooking />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
   );
-}
+};
 
 export default App;
