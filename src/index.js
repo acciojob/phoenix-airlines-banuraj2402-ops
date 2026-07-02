00c5efd458feb42309b7dafe9e63d0f95996a55e
Banu <banuraj2402@gmail.com>
2026-07-02 14:20:00 +0530

    Updated Phoenix Airlines

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App"; // Updated path
 
 ReactDOM.render(<App />, document.getElementById("root"));
